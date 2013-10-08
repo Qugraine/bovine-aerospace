36ae88e4926f068e36c48c5373f65e38e79adc55
danny.c.bowman <danny.c.bowman@gmail.com>
2013-10-08 14:31:12 +0000

    Updated rNOMADS vignette to  include air density Working on trial solution to determine properties at given location for furiousblue

--- furiousblue/MakeProfiles.R
+++ furiousblue/MakeProfiles.R
@@ -19,6 +19,12 @@ variables <- c("TMP")
 hrs.ahead <- 0
 fcst.date <- Sys.time() + 3600 * hrs.ahead
 model.domain <- c(-80, -79, 36.5, 35.5) #Research triangle region
+lat.res <- 0.5 #Resolution in degrees latitude
+lon.res <- 0.5 #Resolution in degrees longitude
+cart.res <- 2000 #Cartesian resolution of interpolated data, in meters
+center.point <- c(mean(model.domain[1:2]), mean(model.domain[3:4])) #Projection point
+
+point.coords <- c(-79.39, 35.51) #Initial coordinates of point of interest
 
 #Get back forecast
 backinfo <- GribGrab(levels, variables, which.fcst = "back", 
@@ -29,3 +35,19 @@ backinfo <- GribGrab(levels, variables, which.fcst = "back",
 foreinfo <- GribGrab(levels, variables, which.fcst = "forward",
    fcst.date = fcst.date, file.name = "fcst_fore.grb",
    model.domain = model.domain)
+
+## Make into model grid
+back.data <- ReadGrib(backinfo$file.name, variables, levels)
+fore.data <- ReadGrib(backinfo$file.name, variables, levels)
+
+back.grd <- ModelGrid(back.data, lat.res, lon.res)
+fore.grd <- ModelGrid(back.data, lat.res, lon.res)
+
+## REGRID FOR HIGHER RESOLUTION
+source("HighResLayers.R")
+back.grd.int <- MakeLayerGrids(back.grd, cart.res, center.point)
+fore.grd.int <- MakeLayerGrids(fore.grd, cart.res, center.point)
+
+#PROJECT LOCATION OF POINT OF INTEREST
+#Note - need to provide cartesian grid here to figure out where object is
+point.xy <- GLOB.XY(point.coords[2], point.coords[1], back.grd.int$projection)
