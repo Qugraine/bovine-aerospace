5ed47eaa7d4162232e118314ed69bf51fade303d
danny.c.bowman <danny.c.bowman@gmail.com>
2013-10-09 15:20:31 +0000

    Now able to find weather profile for a specific point on a horizontal interpolated grid

--- furiousblue/MakeProfiles.R
+++ furiousblue/MakeProfiles.R
@@ -14,8 +14,8 @@ library(rNOMADS) #Interface with GFS forecast
 library(MBA) #Spatial interpolation routines
 library(GEOmap) #Map projection
 
-levels <- c("1000 mb")
-variables <- c("TMP")
+levels <- c("1000 mb", "100 mb")
+variables <- c("TMP", "UGRD")
 hrs.ahead <- 0
 fcst.date <- Sys.time() + 3600 * hrs.ahead
 model.domain <- c(-80, -79, 36.5, 35.5) #Research triangle region
@@ -38,10 +38,10 @@ foreinfo <- GribGrab(levels, variables, which.fcst = "forward",
 
 ## Make into model grid
 back.data <- ReadGrib(backinfo$file.name, variables, levels)
-fore.data <- ReadGrib(backinfo$file.name, variables, levels)
+fore.data <- ReadGrib(foreinfo$file.name, variables, levels)
 
 back.grd <- ModelGrid(back.data, lat.res, lon.res)
-fore.grd <- ModelGrid(back.data, lat.res, lon.res)
+fore.grd <- ModelGrid(fore.data, lat.res, lon.res)
 
 ## REGRID FOR HIGHER RESOLUTION
 source("HighResLayers.R")
@@ -51,3 +51,18 @@ fore.grd.int <- MakeLayerGrids(fore.grd, cart.res, center.point)
 #PROJECT LOCATION OF POINT OF INTEREST
 #Note - need to provide cartesian grid here to figure out where object is
 point.xy <- GLOB.XY(point.coords[2], point.coords[1], back.grd.int$projection)
+
+##GET PROFILE FOR POINT OF INTEREST
+
+#Find which node our point is located in by minimizing absolute distance
+
+abs.x.dist <- abs(point.xy$x - back.grd.int$x)
+abs.y.dist <- abs(point.xy$y - back.grd.int$y)
+
+#Find index of that node
+x.point.ind <- which(abs.x.dist == min(abs.x.dist))
+y.point.ind <- which(abs.y.dist == min(abs.y.dist))
+
+#Get back and fore data for that point
+back.profile <- back.grd.int$z[, , x.point.ind, y.point.ind]
+fore.profile <- fore.grd.int$z[, , x.point.ind, y.point.ind]
