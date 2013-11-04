cc51ab57429a5cf938baa7d3870cabfc112d039f
danny.c.bowman <danny.c.bowman@gmail.com>
2013-11-04 02:42:59 +0000

    Continue working on tracking code.

--- furiousblue/HighResLayers.R
+++ furiousblue/HighResLayers.R
@@ -1,5 +1,45 @@
 library(MBA)
 library(GEOmap)
+
+CombinedProfile <- function(point.coords, back.grd.int, fore.grd.int, weight.avg) {
+   #This function takes the high resolution back and foreward interpolated grids
+   #and produces a single profile (interpolated in space and time) for a given
+   #particle position
+   #INPUTS
+   #    POINT.COORDS - lat/lon coords of point of interest
+   #    BACK.GRD.INT - the model data before the particle time
+   #    FORE.GRD.INT - the model data after the particle time
+   #    WEIGHT.AVG - The weights (time gap) between models
+   #
+   #OUTPUTS
+   #    COMBINED.PROFILE - An atmospheric profile with rows as layers and columns as variables
+
+   #PROJECT LOCATION OF POINT OF INTEREST
+   #Note - need to provide cartesian grid here to figure out where object is
+   point.xy <- GLOB.XY(point.coords[2], point.coords[1], back.grd.int$projection)
+
+    ##GET PROFILE FOR POINT OF INTEREST
+
+    #Find which node our point is located in by minimizing absolute distance
+
+    abs.x.dist <- abs(point.xy$x - back.grd.int$x)
+    abs.y.dist <- abs(point.xy$y - back.grd.int$y)
+
+    #Find index of that node
+    x.point.ind <- which(abs.x.dist == min(abs.x.dist))
+    y.point.ind <- which(abs.y.dist == min(abs.y.dist))
+
+    #Get back and fore data for that point
+    back.profile <- back.grd.int$z[, , x.point.ind, y.point.ind]
+    fore.profile <- fore.grd.int$z[, , x.point.ind, y.point.ind]
+
+    #Make weighted profile by date
+    combined.profile <- (back.profile * weight.avg[1] + fore.profile * weight.avg[2])/sum(weight.avg)
+   
+    combined.profile <- combined.profile[ order(combined.profile[,1]), ] 
+    invisible(combined.profile)
+}
+
 MakeLayerGrids <- function(fcst.grid, resolution, center.point) {
     #This function takes the ModelGrid output and regrids it at a given resolution projected from a given center.point
     #INPUTS
