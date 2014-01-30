b03d153acbf004f520f13e2a7ebe4bd122e2bfe9
danny.c.bowman <danny.c.bowman@gmail.com>
2014-01-30 01:53:25 +0000

    Fixed variable names

--- rNOMADS/rNOMADS/R/get_infra_profiles.R
+++ rNOMADS/rNOMADS/R/get_infra_profiles.R
@@ -18,6 +18,7 @@ for(k in seq_len(length(myTA$lat))) {
         spatial.average = FALSE, temporal.average = FALSE)
     gfs.data <- cbind(as.numeric(unlist(str_extract_all(prof$levels, "\\d+"))) * 100, prof$profile.data)
     atmos.prof <- list(data = gfs.data, 
-        variables = c("Temp (K)", "Zonal Wind (m/s)", "Meridional Wind (m/s)", "Geopotential Height (m)"), forecast.date = forecast.date, lat = lat, lon = lon)
+        variables = c("Pressure (Pa)",  "Geopotential Height (m)", "Temp (K)", "Zonal Wind (m/s)", "Meridional Wind (m/s)"), forecast.date = forecast.date, lat = lat, lon = lon)
     save(file = paste0(myTA$name[k], ".RData"), atmos.prof)
+    break
 }
