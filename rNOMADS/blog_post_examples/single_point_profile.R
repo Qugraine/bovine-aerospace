952f3c9dcba93c58f017b0886cd1780838691450
danny.c.bowman <danny.c.bowman@gmail.com>
2014-03-22 15:31:57 +0000

    Blog examples are ready to go, just need to make images and post.

--- rNOMADS/blog_post_examples/single_point_profile.R
+++ rNOMADS/blog_post_examples/single_point_profile.R
@@ -47,3 +47,34 @@ back.wt <- abs(forecasts$back.hr)/total.time
 
 #Final profile
 profile.array <- (back.profile$profile.data[[1]] * back.wt + fore.profile$profile.data[[1]] * fore.wt)
+
+#Make nice spline curves
+tmp.spline <- splinefun(profile.array[,1], profile.array[,2] - 273.15, method = "natural")
+zone.spline <- splinefun(profile.array[,1], profile.array[,3] * 3.6, method = "natural")
+merid.spline <- splinefun(profile.array[,1], profile.array[,4] * 3.6, method = "natural")
+synth.hgt <- seq(min(profile.array[,1]), max(profile.array[,1]), length.out = 1000)
+synth.tmp <- tmp.spline(synth.hgt)
+synth.zone <- zone.spline(synth.hgt)
+synth.merid <- merid.spline(synth.hgt)
+
+#Make plots
+#Temperature
+plot(synth.tmp, synth.hgt, type = "l",
+   xlab = "Temperature (C)", ylab = "Geopotential Height (m)", 
+   main = paste("Temperature Profile at Sakura-Jima,", forecast.date))
+points(profile.array[,2] - 273.15, profile.array[,1], col = "red")
+#Zonal/Meridional wind speed
+wind.range <- c(min(cbind(synth.zone, synth.merid)),
+   max(cbind(synth.zone, synth.merid)))
+plot(wind.range, c(min(synth.hgt), max(synth.hgt)), type = "n",
+   xlab = "Wind Speed (km/hr)", ylab = "Geopotential Height (m)",
+   main = paste("Zonal and Meridional Wind Speeds at Sakura-Jima,", forecast.date))
+
+points(profile.array[,3] * 3.6, profile.array[,1], col = "red")
+lines(synth.zone, synth.hgt, col = "red")
+points(profile.array[,4] * 3.6, profile.array[,1], pch = 2, col = "blue")
+lines(synth.merid, synth.hgt, col = "blue")
+
+legend("topright", pch = c(1, 2), col = c("red", "blue"), lty = c(1, 1),
+   legend = c("Zonal Wind", "Meridional Wind"))
+
