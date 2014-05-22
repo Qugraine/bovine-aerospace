686ebb8b04524b2d7c56953e4fb84661adb30763
danny.c.bowman@gmail.com <danny.c.bowman@gmail.com@0d62a3c2-f93a-fd45-e7f9-236f6ec13455>
2014-05-22 18:25:58 +0000

    Fixed a bug in rNOMADS and added a directory for the search for Jake 8

---
+++ trunk/jake8_recovery/get_gfs_on_launch_day.R
@@ -0,0 +1,23 @@
+#Get the GFS forecasts for launch day when we lost Jake 8
+library(rNOMADS)
+#Launch date and time, in GMT
+forecast.date <- "2014-05-21 10:00:00 UTC"
+model.date <- 2014052106
+depth <- 10
+forecasts <- GetClosestGFSForecasts(forecast.date, model.date, depth)
+
+model.domain <- c(-81.685181, -74.807739, 36.615528, 33.155948)
+pressure <- c(1, 2, 3, 5, 7,
+10, 20, 30, 50, 70,
+seq(100, 1000, by = 25))
+levels <- paste(pressure, " mb", sep = "")
+variables <- c("HGT", "TMP", "UGRD", "VGRD", "RH")
+
+#Download back forecast
+grib.info <- GribGrab(forecasts$model.url, forecasts$back.forecast,
+   levels, variables, file.name = "back_forecast.grb")
+
+#Download fore forecast
+grib.info <- GribGrab(forecasts$model.url, forecasts$fore.forecast,
+   levels, variables, file.name = "fore_forecast.grb")
+
