95403b2e11c3494b390d00c6cb6593617a7077d7
danny.c.bowman@gmail.com <danny.c.bowman@gmail.com@0d62a3c2-f93a-fd45-e7f9-236f6ec13455>
2014-05-31 01:03:16 +0000

    Slight change to getting weather data - the jake 8 script needs to be run before june 30

--- trunk/jake8_recovery/get_weather_on_launch_day.R
+++ trunk/jake8_recovery/get_weather_on_launch_day.R
@@ -1,15 +1,18 @@
 #Get the GFS forecasts for launch day when we lost Jake 8
 library(rNOMADS)
+
+#GET GFS
+
 #Launch date and time, in GMT
 forecast.date <- "2014-05-21 10:00:00 UTC"
 model.date <- 2014052106
-depth <- 10
+depth <- NULL
 forecasts <- GetClosestGFSForecasts(forecast.date, model.date, depth)
 
 model.domain <- c(-81.685181, -74.807739, 36.615528, 33.155948)
 pressure <- c(1, 2, 3, 5, 7,
 10, 20, 30, 50, 70,
-seq(100, 1000, by = 25))
+seq(100, 1000, by = 25), "10 m above ground", "80 m above ground", "100 m above ground")
 levels <- paste(pressure, " mb", sep = "")
 variables <- c("HGT", "TMP", "UGRD", "VGRD", "RH")
 
